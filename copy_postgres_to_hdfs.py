4282852b42e75a848eb37b2a7c7f76e48a3cf960
Alabai5 <86009186+Alabai5@users.noreply.github.com>
2021-07-26 21:22:05 +0300

    Update copy_postgres_to_hdfs.py

--- copy_postgres_to_hdfs.py
+++ copy_postgres_to_hdfs.py
@@ -1,53 +1,60 @@
-import json
-import os
-from airflow import DAG
-from airflow.operators.postgres_operator import PostgresOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 from datetime import timedelta
+from datetime import date
+import json
+import requests
+import json
+import os
+import yaml
+from requests.exceptions import HTTPError
 
-import psycopg2
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.http_operator import SimpleHttpOperator
 from hdfs import InsecureClient
 
-pg_creds = {
-    'host': '172.20.10.7',
-    'port': '5432',
-    'database':'dshop',
-    'user':'pguser',
-    'password':'secret'
-}
-def main(table_name):
+
+
+
+def main(params=date.today()):
     client = InsecureClient(f'http://127.0.0.1:50070', user='user')
     #create directiry in HDFS
-    client.makedirs('/bronze/dshop/{0}'.format(table_name))
+    client.makedirs('/bronze/RobotDreamsAPI/{0}'.format(params))
 
-    #copy table's data from PostgreSQL to HDFS
-    client.delete(os.path.join ('/','/bronze/dshop/{0}'.format(table_name),'{0}.csv'.format(table_name)), recursive=False)
-    with psycopg2.connect(**pg_creds) as pg_connection:
-        cursor = pg_connection.cursor()
-        with client.write(os.path.join ('/','/bronze/dshop/{0}'.format(table_name),'{0}.csv'.format(table_name)),) as csv_file:
-            cursor.copy_expert('COPY (SELECT * FROM {0}) TO STDOUT WITH HEADER CSV'.format(table_name), csv_file)
+    url = "https://robot-dreams-de-api.herokuapp.com/auth"
+    headers = {"content-type": "application/json"}
+    data = {"username": "rd_dreams", "password": "djT6LasE"}
+    r = requests.post(url, headers=headers, data=json.dumps(data))
+    token = r.json()['access_token']
+
+    client.delete(os.path.join('/', '/bronze/RobotDreamsAPI/{0}'.format(params), f'Product[{params}].json'), recursive=False)
+
+    try:
+        #os.makedirs(os.path.join("./data/", date), exist_ok=True)
+        url = "https://robot-dreams-de-api.herokuapp.com/out_of_stock"
+        headers = {"content-type": "application/json", "Authorization": "JWT " + token}
+        d = {"date": params}
+        response = requests.get(url, headers=headers, data=json.dumps(d, default=str))
+        response.raise_for_status()
+        data = response.json()
+        client.write(os.path.join('/', '/bronze/RobotDreamsAPI/{0}'.format(params), f'Product-{params}.json'), data=json.dumps(data), encoding='utf-8')
+
+    except HTTPError:
+        print('Date does not exists in the API')
+    print(date)
 
 dag_rd = DAG(
-    dag_id='copy_data_from_postgreSQL_to_hdfs',
-    description='connection to postgres DB and load data to hdfs',
+    dag_id='copy_data_from_API_to_hdfs',
+    description='connection to robot dreams API',
     start_date=datetime(2021, 7, 18, 1, 0),
-    end_date=datetime(2022, 7, 23, 5, 0),
+    end_date=datetime(2022, 7, 23, 10, 0),
     schedule_interval='@daily',
-    params={'list_tables':['aisles', 'clients', 'departments', 'orders', 'products']}
 )
 
-for table_name in dag_rd.params['list_tables']:
-    copy_data = PythonOperator(
-        task_id=f"CopyToHDFS_{table_name}",
-        dag=dag_rd,
-        python_callable= main,
-        op_kwargs={
-            "table_name": table_name
-        }
+run_connection_to_api = PythonOperator(
+    task_id='get_data_from_API',
+    python_callable=main,
+    dag=dag_rd
+)
 
-
-    )
-
-copy_data
+run_connection_to_api
